3ed6193c0a059f1b4746c2329f72735a8933a7ee
d-h-morrison <d_h_morrison@outlook.com>
2015-06-04 08:22:42 -0400

    Repaired routing states.

--- app/app.js
+++ app/app.js
@@ -14,6 +14,7 @@
                 "$urlRouterProvider",
                 function($stateProvider, $urlRouterProvider)
                 {
+
                     // Default route state.
                     $urlRouterProvider.otherwise("/");
 
@@ -26,6 +27,7 @@
 
                         // Products state.
                         .state("productList",{
+                            // DO NOT OMIT THE "/" URL PREFIX OR THE ROUTE WILL NOT WORK.
                             url: "/products",
                             templateUrl: "app/products/productListView.html",
                             controller: "ProductListCtrl as vm"
@@ -33,11 +35,19 @@
 
                         // Edit state.
                         .state("productEdit",{
-                            url: "products/edit/:productId",
+                            // DO NOT OMIT THE "/" URL PREFIX OR THE ROUTE WILL NOT WORK.
+                            url: "/products/edit/:productId", // Parameter passed to the state.
                             templateUrl:"app/products/productEditView.html",
                             controller: "ProductEditCtrl as vm"
                         })
 
+                        // Product detail state.
+                        .state("productDetail",{
+                            // DO NOT OMIT THE "/" URL PREFIX OR THE ROUTE WILL NOT WORK.
+                            url: "/products/:productId", // Parameter passed to the state.
+                            templateUrl:"app/products/productDetailView.html",
+                            controller: "ProductDetailCtrl as vm"
+                        })
 
                 }]
 
--- common/services/productResourceMock.js
+++ common/services/productResourceMock.js
@@ -38,6 +38,18 @@
                 "imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
             },
             {
+                "productId": 2,
+                "productName": "Garden Cart",
+                "productCode": "GDN-0023",
+                "releaseDate": "March 18, 2010",
+                "description": "15 gallon capacity rolling garden cart",
+                "cost": 20.00,
+                "price": 32.99,
+                "category": "garden",
+                "tags": ["barrow", "cart", "wheelbarrow"],
+                "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
+            },
+            {
                 "productId": 6,
                 "productName": "Russian T-14 Armata",
                 "productCode": "RT14-0945",
